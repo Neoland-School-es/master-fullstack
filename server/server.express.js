31304589793ebb02061215c440529802dd8e47d5
Alvaro Isorna <alvaro.isorna@gmail.com>
2025-03-04 13:13:50 +0100

    feat(practicas): :sparkles: ExpressJS + CRUD

--- server/server.express.js
+++ server/server.express.js
@@ -1,26 +1,44 @@
 // /server/server.express.js
 import express from 'express';
+import bodyParser from 'body-parser';
+import { crud } from "./server.crud.js";
 
 const app = express();
 const port = process.env.PORT;
+const ARTICLES_URL = './server/BBDD/articles.json'
 
 // Static server
 app.use(express.static('practica'));
+// for parsing application/json
+app.use(bodyParser.json())
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 // CREATE
 app.post('/create/articles', requireAuth, (req, res) => {
-  res.send('Article created!')
+  crud.create(ARTICLES_URL, req.body, (data) => {
+    console.log(`server create article ${data.name} creado`, data)
+    res.json(data)
+  });
 })
 // READ
 app.get('/read/articles', (req, res) => {
-  res.send('Articles\' list')
+  crud.read(ARTICLES_URL, (data) => {
+    console.log('server read articles', data)
+    res.json(data)
+  });
 })
 // UPDATE
 app.put('/update/articles/:id', requireAuth, (req, res) => {
-  res.send(`Article ${req.params.id} updated!`)
+  crud.update(ARTICLES_URL, req.params.id, req.body, (data) => {
+    console.log(`server update article ${req.params.id} modificado`, data)
+  });
 })
 // DELETE
 app.delete('/delete/articles/:id', requireAuth, (req, res) => {
-  res.send(`Article ${req.params.id} deleted!`)
+  crud.delete(ARTICLES_URL, req.params.id, (data) => {
+    console.log('server delete article', req.params.id, data)
+    res.json(data)
+  })
 })
 
 app.listen(port, async () => {
