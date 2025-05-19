1a2e8284223c5b0ef855cd2d4a8d34fd61b5b311
Alvaro Isorna <alvaro.isorna@gmail.com>
2025-05-19 13:08:40 +0200

    feat(express): uso de plantillas con handlebars

--- server/express-templates/routes/users.js
+++ server/express-templates/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respondemos con un texto plano');
 });
 
 module.exports = router;
